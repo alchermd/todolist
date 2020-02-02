1ffa344fdc570d651d70cf993ff0226dac560822
John Alcher <johnalcherdoloiras@gmail.com>
2020-02-02 16:34:26 +0800

    Implement updating of tasks

--- src/main/java/com/example/todolist/TaskController.java
+++ src/main/java/com/example/todolist/TaskController.java
@@ -1,9 +1,13 @@
 package com.example.todolist;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/tasks")
@@ -14,5 +18,21 @@ public class TaskController {
     @GetMapping("/")
     public Iterable<Task> getAllTasks() {
         return taskRepository.findAll();
+    }
+
+    @PutMapping("/{taskId}/")
+    public Task updateTask(@PathVariable Integer taskId, @RequestBody Map<String, Object> payload) {
+        Task task = taskRepository.findById(taskId)
+                .orElse(null);
+
+        if (task == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task does not exist");
+        }
+
+        task.setBody((String) payload.get("body"));
+        task.setFinished((Boolean) payload.get("finished"));
+        taskRepository.save(task);
+
+        return task;
     }
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,7 +7,25 @@ var app = new Vue({
         fetch('/tasks/')
             .then(response => response.json())
             .then(tasks => {
-                this.tasks = tasks;
+                this.tasks = tasks.sort(this.compareById);
             });
-    }
+    },
+    methods: {
+        updateTask(event, task) {
+            fetch(`/tasks/${task.id}/`, {
+                method: 'put',
+                body: JSON.stringify(task),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+        },
+        compareById(a, b) {
+            if (a.id < b.id) {
+                return -1;
+            }
+
+            return 1;
+        },
+    },
 });
